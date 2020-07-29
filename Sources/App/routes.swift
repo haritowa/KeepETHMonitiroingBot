c54c3f66e537a4b6bd7bfb0e78950863f79e7613
haritowa <haritowa@icloud.com>
2020-07-29 04:58:18 +0300

    Update docker deploy

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    app.get("hello") { _ in "Hello" }
+    
     app.post("telegram", "webhook", .constant(app.telegramBotAPIKey), use: TelegramWebhookController.handle)
 }
