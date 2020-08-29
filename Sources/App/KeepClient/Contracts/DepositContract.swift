0b313e49ccd1d5412c6f5063319d75a3940f767d
haritowa <haritowa@icloud.com>
2020-08-30 02:01:04 +0300

    Add get operators and get depositState methods to keep client

--- Sources/App/KeepClient/Contracts/DepositContract.swift
+++ Sources/App/KeepClient/Contracts/DepositContract.swift
@@ -17,4 +17,26 @@ class DepositContract: GenericERC20Contract {
         let method = SolidityConstantFunction(name: "keepAddress", inputs: inputs, outputs: outputs, handler: self)
         return method.invoke()
     }
+    
+    var collateralizationPercentage: SolidityInvocation {
+        let inputs = [SolidityFunctionParameter]()
+        let outputs = [SolidityFunctionParameter(name: "_collateralizationPercentage", type: .uint256)]
+        let method = SolidityConstantFunction(name: "collateralizationPercentage", inputs: inputs, outputs: outputs, handler: self)
+        return method.invoke()
+    }
+    
+    var undercollateralizedThresholdPercent: SolidityInvocation {
+        let inputs = [SolidityFunctionParameter]()
+        let outputs = [SolidityFunctionParameter(name: "_undercollateralizedThresholdPercent", type: .uint16)]
+        let method = SolidityConstantFunction(name: "undercollateralizedThresholdPercent", inputs: inputs, outputs: outputs, handler: self)
+        return method.invoke()
+    }
+    
+    var severelyUndercollateralizedThresholdPercent: SolidityInvocation {
+        let inputs = [SolidityFunctionParameter]()
+        let outputs = [SolidityFunctionParameter(name: "_severelyUndercollateralizedThresholdPercent", type: .uint16)]
+        let method = SolidityConstantFunction(name: "severelyUndercollateralizedThresholdPercent", inputs: inputs, outputs: outputs, handler: self)
+        return method.invoke()
+    }
 }
+
--- Sources/App/KeepClient/KeepClient.swift
+++ Sources/App/KeepClient/KeepClient.swift
@@ -16,11 +16,20 @@ enum KeepTBTCAuthResult {
     case uknownError
 }
 
+enum DepostCollateralizationState {
+    case normal
+    case undercollateralized
+    case severelyUndercollateralized
+}
+
 protocol KeepClientProtocol {
     var eventLoop: EventLoop { get }
     
     func unbondedValue(operatorAddress: EthereumAddress) -> EventLoopFuture<BigUInt>
     func hasTBTCAuthorization(operatorAddress: EthereumAddress) -> EventLoopFuture<KeepTBTCAuthResult>
+    func depositState(depositAddress: EthereumAddress) -> EventLoopFuture<DepostCollateralizationState>
+    
+    func operators(for depositAddress: EthereumAddress) -> EventLoopFuture<[EthereumAddress]>
 }
 
 class KeepClient: KeepClientProtocol {
@@ -49,6 +58,26 @@ class KeepClient: KeepClientProtocol {
         }
     }
     
+    func depositState(depositAddress: EthereumAddress) -> EventLoopFuture<DepostCollateralizationState> {
+        let current = collateralizationPercentage(for: depositAddress).map(UInt.init)
+        let firstGrade = undercollateralizedThresholdPercent(for: depositAddress).map(UInt.init)
+        let secondGrade = severelyUndercollateralizedThresholdPercent(for: depositAddress).map(UInt.init)
+        
+        return EventLoopFuture.whenAllSucceed([current, firstGrade, secondGrade], on: eventLoop).map { items in
+            let currentValue = items[0]
+            let firstGradeValue = items[1]
+            let secondGradeValue = items[2]
+            
+            if currentValue < secondGradeValue {
+                return DepostCollateralizationState.severelyUndercollateralized
+            } else if currentValue < firstGradeValue {
+                return DepostCollateralizationState.undercollateralized
+            } else {
+                return DepostCollateralizationState.normal
+            }
+        }
+    }
+    
     private func getTBTCSoritonPool(for operatorAddress: EthereumAddress) -> EventLoopFuture<EthereumAddress> {
         web3.eth.Contract(type: BondedECDSAKeepFactoryContract.self, address: BondedECDSAKeepFactoryContract.testNetAddress)
             .getSortitionPool(applicationAddress: TBTCSystemContract.testNetAddress)
@@ -65,6 +94,39 @@ class KeepClient: KeepClientProtocol {
         hasContractAuthorization(for: operatorAddress, sortitionPool: sortitionPool)
             .map { $0 ? KeepTBTCAuthResult.granted : KeepTBTCAuthResult.notAuthorized }
             .flatMapErrorThrowing { _ in KeepTBTCAuthResult.uknownError }
+    }
+    
+    func operators(for depositAddress: EthereumAddress) -> EventLoopFuture<[EthereumAddress]> {
+        let deposit = self.deposit(for: depositAddress)
+        
+        let keepAddress: EventLoopFuture<EthereumAddress> = deposit.keepAddress.callAndExtract(eventLoop: eventLoop)
+        
+        return keepAddress
+            .map { BondedECDSAKeepContract(address: $0, eth: self.web3.eth) }
+            .flatMap { contract -> EventLoopFuture<[EthereumAddress]> in contract.members.callAndExtract(eventLoop: self.eventLoop) }
+    }
+    
+    // MARK: - Depost Collateralization
+    private func deposit(for depositAddress: EthereumAddress) -> DepositContract {
+        DepositContract(address: depositAddress, eth: web3.eth)
+    }
+    
+    private func collateralizationPercentage(for depositAddress: EthereumAddress) -> EventLoopFuture<BigUInt> {
+        deposit(for: depositAddress)
+            .collateralizationPercentage
+            .callAndExtract(eventLoop: eventLoop)
+    }
+    
+    private func undercollateralizedThresholdPercent(for depositAddress: EthereumAddress) -> EventLoopFuture<UInt16> {
+        deposit(for: depositAddress)
+            .undercollateralizedThresholdPercent
+            .callAndExtract(eventLoop: eventLoop)
+    }
+    
+    private func severelyUndercollateralizedThresholdPercent(for depositAddress: EthereumAddress) -> EventLoopFuture<UInt16> {
+        deposit(for: depositAddress)
+            .severelyUndercollateralizedThresholdPercent
+            .callAndExtract(eventLoop: eventLoop)
     }
 }
 
