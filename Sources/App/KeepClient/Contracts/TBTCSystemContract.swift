ace73360ad315ca3eb3672ad3293866415f63625
haritowa <haritowa@icloud.com>
2020-09-03 22:20:52 +0300

    Update contracts

--- Sources/App/KeepClient/Contracts/TBTCSystemContract.swift
+++ Sources/App/KeepClient/Contracts/TBTCSystemContract.swift
@@ -11,7 +11,7 @@ import Web3
 import Web3ContractABI
 
 class TBTCSystemContract: GenericERC20Contract {
-    static let testNetAddress = EthereumAddress(hexString: "0x9F3B3bCED0AFfe862D436CB8FF462a454040Af80")!
+    static let testNetAddress = EthereumAddress(hexString: "0xc3f96306eDabACEa249D2D22Ec65697f38c6Da69")!
     
     open override var events: [SolidityEvent] {
         var superEvents = super.events
